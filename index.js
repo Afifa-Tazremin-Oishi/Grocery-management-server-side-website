a8be3cec4dfbe40605e3b6a74aaf1393d57cf976
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.con>
2022-05-29 11:39:55 +0600

    second

--- index.js
+++ index.js
@@ -22,3 +22,129 @@ connectDB((err) => {
 });
 
 
+
+app.get("/products", (req, res) => {
+    let products = [];
+
+    db.collection("products")
+        .find()
+        .forEach((product) => products.push(product))
+        .then(() => res.status(200).json(products))
+        .catch((error) =>
+            res.status(500).json({ error: "Could not fetch the documents" })
+        );
+});
+
+app.get("/products/limits", (req, res) => {
+    let products = [];
+
+    db.collection("products")
+        .find()
+        .limit(6)
+        .forEach((product) => products.push(product))
+        .then(() => res.status(200).json(products))
+        .catch((error) =>
+            res.status(500).json({ error: "Could not fetch the documents" })
+        );
+});
+
+app.get("/products/myProducts", verifyJWT, (req, res) => {
+    const email = req.query;
+    const decodedEmail = req.decoded.email;
+
+
+    if (email.email === decodedEmail) {
+        let products = [];
+
+        db.collection("products")
+            .find(email)
+            .forEach((product) => products.push(product))
+            .then(() => res.status(200).json(products))
+            .catch((error) =>
+                res.status(500).json({ error: "Could not fetch the documents" })
+            );
+    } else {
+        res.status(403).send({ message: "Forbidden Message" });
+    }
+});
+
+app.post("/products", (req, res) => {
+    const product = req.body;
+    // console.log(product);
+    console.log("Add Product: ", product);
+    db.collection("products")
+        .insertOne(product)
+        .then((result) => res.status(201).json(result))
+        .catch((err) =>
+            res.status(500).json({ error: "Could not create new document" })
+        );
+});
+
+app.get("/products/:id", (req, res) => {
+    console.log("Single Product Request Id: ", req.params.id);
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection("products")
+            .findOne({ _id: ObjectId(req.params.id) })
+            .then((result) => res.status(200).json(result))
+            .catch((err) =>
+                res.status(500).json({ error: "Could not fetch the document" })
+            );
+    } else {
+        res.status(500).json({ error: "Not valid document ID" });
+    }
+});
+
+app.patch("/products/:id/qty", (req, res) => {
+    console.log("Updating Reuest ID: ", req.params.id);
+
+    const { addQty } = req.body;
+    console.log(addQty);
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection("products")
+            .updateOne(
+                { _id: ObjectId(req.params.id) },
+                { $inc: { quantity: addQty } }
+            )
+            .then((result) => res.status(200).json(result))
+            .catch((err) =>
+                res.status(500).json({ error: "Could not update the document" })
+            );
+    } else {
+        res.status(500).json({ error: "Not valid document ID" });
+    }
+});
+
+app.patch("/products/:id", (req, res) => {
+    console.log("Updating Reuest ID: ", req.params.id);
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection("products")
+            .updateOne(
+                { _id: ObjectId(req.params.id) },
+                { $inc: { quantity: -1, sold: 1 } }
+            )
+            .then((result) => res.status(200).json(result))
+            .catch((err) =>
+                res.status(500).json({ error: "Could not update the document" })
+            );
+    } else {
+        res.status(500).json({ error: "Not valid document ID" });
+    }
+});
+
+app.delete("/products/:id", (req, res) => {
+    console.log("Delete Request Id: ", req.params.id);
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection("products")
+            .deleteOne({ _id: ObjectId(req.params.id) })
+            .then((result) => res.status(200).json(result))
+            .catch((err) =>
+                res.status(500).json({ error: "Could not delete the document" })
+            );
+    } else {
+        res.status(500).json({ error: "Not valid document ID" });
+    }
+});
+
+
